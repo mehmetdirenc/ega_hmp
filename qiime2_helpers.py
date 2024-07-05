76d325b7e3b818cd52958b4694f9db3bd6ea7753
mehmetdirenc <mehmetdirencmungan@gmail.com>
2024-07-05 17:06:42 +0200

    share

--- qiime2_helpers.py
+++ qiime2_helpers.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import pandas as pd
+import itertools
 from parse_metadata import *
 
 
@@ -34,6 +36,38 @@ def parse_seq_to_taxa(tax_tsv):
             feature_id_taxa_dict[feature_id] = full_tax
 
 
+def parse_seq_to_taxa(tax_tsv):
+    feature_id_taxa_dict = {}
+    with open(tax_tsv, "r") as tax_feature:
+        for line in tax_feature:
+            if line.startswith("Feature") or line.startswith("#"):
+                continue
+            split_line = line.split("\t")
+            feature_id = split_line[0]
+            full_tax = split_line[1]
+            feature_id_taxa_dict[feature_id] = full_tax
+    return feature_id_taxa_dict
+
+def sample_taxa_abundance(qzv_table_metadata_file):
+    df = pd.read_csv(qzv_table_metadata_file, sep='\t', header=[0])
+    df = df[~df.id.str.startswith('#')]
+    return df
+
+
+
+def abundance_boi(qzv_table_metadata_file, tax_tsv):
+    boi_features = {}
+    boi_features_only_final = {}
+    abundance_df = sample_taxa_abundance(qzv_table_metadata_file)
+    feature_id_taxa_dict = parse_seq_to_taxa(tax_tsv)
+    for feature in feature_id_taxa_dict:
+        if "g__Sarcina" in feature_id_taxa_dict[feature] or "g__Hathewaya" in feature_id_taxa_dict[feature] or  "g__Bacillus" in feature_id_taxa_dict[feature]:
+            boi_features[feature] = feature_id_taxa_dict[feature]
+            boi_features_only_final[feature] = feature_id_taxa_dict[feature].split(";")[-1]
+    df_boi = abundance_df[itertools.chain(["id"], list(boi_features.keys()))]
+    df_boi.rename(columns=boi_features_only_final, inplace=True)
+    df_boi.to_csv('/home/direnc/results/ega/16s/qiime/all_results/boi.tsv', sep="\t")
+    return
 
 
 
@@ -43,6 +77,12 @@ if __name__ == '__main__':
     manifest_filepath = "/mnt/lustre/projects/mager-1000ibd/input/ega/16s/EGAD00001008215/manifest.tsv"
     metadata_filepath = "/mnt/lustre/projects/mager-1000ibd/input/ega/16s/EGAD00001008215/metadata.tsv"
     samples_tsv_path = "/mnt/lustre/projects/mager-1000ibd/datasets/EGAD00001008215/metadata/samples.tsv"
-    create_metadata(manifest_filepath, metadata_filepath, patients_metadata_path)
-    create_sample_to_patient_dict(samples_tsv_path)
+    # abundance_table = "/mnt/lustre/projects/mager-1000ibd/results/ega/16s/EGAD00001008215/qiime/all_results/exported_abundance_table/metadata.tsv"
+    # tax_tsv = "/mnt/lustre/projects/mager-1000ibd/results/ega/16s/EGAD00001008215/qiime/all_results/exported_taxonomy/taxonomy.tsv"
+    abundance_table = "/home/direnc/results/ega/16s/qiime/all_results/exported_abundance_table/metadata.tsv"
+    tax_tsv = "/home/direnc/results/ega/16s/qiime/all_results/exported_taxonomy/taxonomy.tsv"
+    # sample_taxa_abundance(abundance_table)
+    abundance_boi(abundance_table, tax_tsv)
+    # create_metadata(manifest_filepath, metadata_filepath, patients_metadata_path)
+    # create_sample_to_patient_dict(samples_tsv_path)
     print("a")
